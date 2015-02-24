7de65de2962b8bf5d1b8aeb02e92ffc1ecabd13a
Schoenbohm <steven.schoenbohm@comcast.net>
2015-02-24 13:43:35 -0600

    Branch 1

--- test2/Module1.vb
+++ test2/Module1.vb
@@ -3,6 +3,7 @@
     Sub Main()
         Console.WriteLine("test app")
         Console.WriteLine("other line with less text")
+        Console.WriteLine("Branch 1")
         Console.ReadKey()
     End Sub
 
